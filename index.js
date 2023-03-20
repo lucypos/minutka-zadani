fed809a315c34654afeab494883b99ffcbafba3b
Lucie Pospisilova <pospisilova.lucy@gmail.com>
2023-03-20 15:41:29 +0100

    rozpracované

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('funguju!');
+//Minutka
+
+const zvoneni = () => {
+   let cvrkot = document.querySelector('.alarm');
+    cvrkot.classList.add('alarm--ring');
+    console.log('zvoní')
+}
+
+let zvuk = document.querySelector('#zvuk')
+ {
+    zvuk.play(zvoneni)
+}
+let vstupUzivatele = Number(prompt('Kolik vteřin, než minutka začne zvonit?'))
+    let vteriny = vstupUzivatele * 1000
+console.log(vteriny)
+
+const budik = setTimeout(zvoneni, vteriny)
+
