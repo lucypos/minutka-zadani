ac6aa37a939c016c91ca4762cd463b48bbd1caa1
Lucie Pospisilova <pospisilova.lucy@gmail.com>
2023-03-21 15:29:03 +0100

    final

--- index.js
+++ index.js
@@ -4,15 +4,26 @@ const zvoneni = () => {
    let cvrkot = document.querySelector('.alarm');
     cvrkot.classList.add('alarm--ring');
     console.log('zvoní')
+
+let zvuk = document.querySelector('audio')
+zvuk.play()
 }
 
-let zvuk = document.querySelector('#zvuk')
- {
-    zvuk.play(zvoneni)
-}
-let vstupUzivatele = Number(prompt('Kolik vteřin, než minutka začne zvonit?'))
-    let vteriny = vstupUzivatele * 1000
+let vstupUzivatele = Number(prompt('Kolik vteřin, než minutka začne zvonit?'));
+    let vteriny = vstupUzivatele * 1000;
 console.log(vteriny)
 
 const budik = setTimeout(zvoneni, vteriny)
-
+/*
+const preruseniZvoneni = () => {
+    cvrkot.classList.remove ('alarm--ring', 'audio');
+    console.log('Zrušeno')
+}
+*/
+const preruseniZvoneni = document.querySelector('.vypnout');
+vypnout.addEventListener('click', () => {
+  clearTimeout(budik);
+  let cvrkot = document.querySelector('.alarm');
+  cvrkot.classList.remove('alarm--ring');
+  console.log('zrušeno')
+})
